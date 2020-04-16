1cf49037315a2386fd7506d2cc5c2956acb18861
Adrian Alfredo Rojas Duarte <e-aarojas@neoris.com>
2020-04-16 10:02:56 -0600

    front and api work

--- APIdemo/APIdemo/WeatherForecast.cs
+++ APIdemo/APIdemo/WeatherForecast.cs
@@ -1,15 +1,19 @@
+using Newtonsoft.Json;
 using System;
 
 namespace APIdemo
 {
     public class WeatherForecast
     {
+        [JsonProperty(PropertyName = "date")]
         public DateTime Date { get; set; }
 
+        [JsonProperty(PropertyName = "temperatureC")]
         public int TemperatureC { get; set; }
 
+        [JsonProperty(PropertyName = "temperatureF")]
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
+        [JsonProperty(PropertyName = "summary")]
         public string Summary { get; set; }
     }
 }
--- FrontDemo/src/app/app.component.ts
+++ FrontDemo/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherForecastService } from './weather-forecast.service';
+import { WeatherForecast } from './models/weatherForecast';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  items: WeatherForecast[] = [];
   title = 'FrontDemo';
+  constructor(private service: WeatherForecastService) {
+    this.service.getAll().subscribe((res: WeatherForecast[]) => { console.log(res); this.items = res });
+    console.log("asdasd");
+
+  }
+  ngOnInit(): void {
+
+  }
 }
