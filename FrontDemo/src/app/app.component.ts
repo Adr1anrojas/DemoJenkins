25c53e14f06c52b65d40d592577428315403c616
Adrián Rojas <43327484+Adr1anrojas@users.noreply.github.com>
2020-04-17 11:26:19 -0600

    Update app.component.ts

--- FrontDemo/src/app/app.component.ts
+++ FrontDemo/src/app/app.component.ts
@@ -9,7 +9,7 @@ import { WeatherForecast } from './models/weatherForecast';
 })
 export class AppComponent implements OnInit {
   items: WeatherForecast[] = [];
-  title = 'FrontDemoPush3';
+  title = 'FrontDemoPush4';
   constructor(private service: WeatherForecastService) {
     this.service.getAll().subscribe((res: WeatherForecast[]) => { console.log(res); this.items = res });
     console.log("test");
