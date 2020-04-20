b8768913976405f29ae8f9f980873062e56de52f
Adrian Alfredo Rojas Duarte <e-aarojas@neoris.com>
2020-04-20 13:12:33 -0600

    update

--- FrontDemo/cypress/integration/sample_spec.js
+++ FrontDemo/cypress/integration/sample_spec.js
@@ -1,6 +1,6 @@
 describe('Testing the hero list page', function () {
-  it('should contain the word FrontDemo2 ', function () {
+  it('should contain the word FrontDemoPush4 ', function () {
     cy.visit('http://localhost:4200');
-    cy.contains("FrontDemo2 ");
+    cy.contains("FrontDemoPush4 ");
   })
 });
